42e8833ad6a355d58103df31a3181eb430dee47d
Thomas Boyt <thomas@ledgerx.com>
2014-11-04 12:48:06 -0500

    Coquette boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/main.js',
+    vendor: [
+      'coquette',
+    ]
+  },
+
+  output: {
+    path: 'public/',
+    filename: "bundle.js"
+  },
+
+  resolve: {
+    // allow resolve modules that are in node_modules/ before looking into subdirectory
+    // prevent duplicate copies of modules
+    root: require('path').resolve('./node_modules')
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+  ],
+
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'esnext'}
+    ]
+  }
+};
